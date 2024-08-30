a80366eabe7247b77f862a6441c4eb0558bb44d8
santosh302 <santoshkumarsy05@gmail.com>
2024-08-30 17:25:54 +0530

    update

--- Projects/pokemons/src/App.jsx
+++ Projects/pokemons/src/App.jsx
@@ -1,2 +1,14 @@
 import {Route, Routes} from 'react-router-dom'
 import './App.css'
+import Pokedex from './components/Pokedex/Pokedex'
+import PokemonDetails from './components/PokemonDetails/PokemonDetails'
+
+function App(){
+    return (
+        <Routes>
+            <Route path="/" element={<Pokedex />} />
+            <Route path="/pokemon/:id" element={<PokemonDetails />} />
+            <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+    )
+}
